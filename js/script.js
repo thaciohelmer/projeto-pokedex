66e38073313e08345789d7b39f94c3968f984aa1
MKT AUX <mkt-aux@suporte.local>
2022-07-07 14:55:37 -0300

    Atualição funções / interface

--- js/script.js
+++ js/script.js
@@ -5,20 +5,28 @@ let pokemonName = null;
 const btnSearch = document
   .getElementById("btn-search")
   .addEventListener("click", () => {
+    errorClear();
     pokemonName = document.getElementById("pokemonName").value;
     resultSearch(pokemonName.toLowerCase());
   });
 
 /* BUSCA NA API */
 
+function errorClear() {
+  document.getElementById("alert-mensage").innerText = "";
+}
+
 const resultSearch = (pokemonName) =>
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       /* document.getElementById("id").innerText = data["id"]; */
       document.getElementById("infs").innerText = data["name"];
       let img = data["sprites"]["other"]["official-artwork"]["front_default"];
       document.getElementById("img-pokemon").setAttribute("src", img);
     })
-    .catch((error) => console.log(error));
+    .catch(
+      (error) =>
+        (document.getElementById("alert-mensage").innerText =
+          "Nome do pokemon inválido ou incorreto !")
+    );
